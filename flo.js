237102acb0f8c0428f347a6da34ca2eca0cfd7b4
yong <zengjunyong@gmail.com>
2015-12-04 10:32:08 +0800

    fb-flo

--- flo.js
+++ flo.js
@@ -5,20 +5,16 @@ var flo = require('fb-flo'),
 
 var server = flo('./app/', {
   port: 8888,
-  dir: './app/',
-  glob: ['./app/**/*.js', './app/**/*.css']
+  glob: ['**/*.js', '**/*.css']
 }, resolver);
 
-server.once('ready', function() {
+server.once('ready', function () {
   console.log('Ready!');
 });
 
 function resolver(filepath, callback) {
-  exec('make', function (err) {
-    if (err) throw err;
-    callback({
-      resourceURL: 'app' + path.extname(filepath),
-      contents: fs.readFileSync('app' + path.extname(filepath)).toString()
-    })
+  callback({
+    resourceURL: 'app/' + filepath,
+    contents: fs.readFileSync('app/' + filepath)
   });
 }
