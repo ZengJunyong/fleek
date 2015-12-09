7fe108ab1940a2ca8e0877d121e521edb69c6a85
yong <zengjunyong@gmail.com>
2015-12-10 00:18:53 +0800

    the most simplest bar, also it works on UC

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,10 +22,13 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      '../app/bower_components/jquery/dist/jquery.js',
-      '../app/bower_components/angular/angular.js',
-      '../app/bower_components/bootstrap/dist/js/bootstrap.js',
-      '../app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/jquery/dist/jquery.js',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/SidebarTransitions/js/modernizr.custom.js',
+      'app/bower_components/SidebarTransitions/js/classie.js',
+      'app/bower_components/SidebarTransitions/js/sidebarEffects.js',
+      'app/bower_components/bootstrap/dist/js/bootstrap.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
